e64094aa770d791d4debf7f7087bd198377aaecf
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-07-19 12:36:06 +0100

    second commit, wanted to know how it works

--- card.py
+++ card.py
@@ -4,13 +4,15 @@ def luhn_checksum(card_number):
     digits = digits_of(card_number)
     odd_digits = digits[-1::-2]
     even_digits = digits[-2::-2]
-    print(even_digits)
-    print(odd_digits)
+    print(sum(even_digits))
+    print(sum(odd_digits))
     checksum = 0
     checksum += sum(odd_digits)
     for d in even_digits:
         checksum += sum(digits_of(d*2))
+    print(checksum % 10)
     return checksum % 10
+    
 
 print('valid') if luhn_checksum('4532015112830366')==0 else print('invalid')
 
